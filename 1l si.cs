a072ec88a885757b7641aa1865cc142e91f0978e
b-aaz <85005689+b-aaz@users.noreply.github.com>
2022-12-18 02:55:42 +0330

    Update 1l si.cs
    Rewrote with readkey

--- 1l si.cs
+++ 1l si.cs
@@ -1,1 +1,1 @@
-﻿start: Console.WriteLine("1:stone 2:sisor 4:paper");Console.WriteLine((statet)( byte.Parse(Console.ReadLine()) | byte.Parse(Console.ReadLine())) );goto start;
+﻿start: Console.WriteLine("r=Rock t=Paper x=Sisors");Console.WriteLine((statet) ( Console.ReadKey(true).KeyChar|Console.ReadKey(true).KeyChar ) );goto start;
